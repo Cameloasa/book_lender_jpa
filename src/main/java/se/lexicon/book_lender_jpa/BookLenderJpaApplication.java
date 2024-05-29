c8d8031ff012982fb440de7c893f7a7887164825
Camelia Ciuca <camelia1414@gmail.com>
2024-05-29 10:06:17 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/book_lender_jpa/BookLenderJpaApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.book_lender_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLenderJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookLenderJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/book_lender_jpa/BookLenderJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.book_lender_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLenderJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
